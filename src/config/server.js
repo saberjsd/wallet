76b262f4d0f420e778739d8709e871177f26606a
DjgMyGit <643716244@qq.com>
2018-07-11 18:08:45 +0800

    change  remote port

--- src/config/server.js
+++ src/config/server.js
@@ -1,11 +1,6 @@
 const api={
-     dappId:"d14afe4b77487172be202e6d2d696012b51bf10c52c5e0903d9209e649d757c0",
-     baseUrl:'http://47.91.223.85:8191',
-    //baseUrl:'',
-    //dappId:"2ef34add7fc61018835043ac9d4cb6aa1f61e916d6a986ea4c7e5fa4f8810939",
-    //baseUrl:'http://192.168.1.15:4097',
-    // dappId:"63863696d786c0b8d25af17d89c2f02e04376e616baf29a1195685e379954d79",
-    // baseUrl:"http://211.149.178.97:80"
+     dappId: "1847c9d186b72963fff6fa8d5c9d26d534c32d4d65dc984e09b881ef71bd6409",
+     baseUrl: 'http://47.75.158.207:8192',
 }
 const dappBaseUrl=api.baseUrl+"/api/dapps/"+api.dappId;
 const magic_test="594fe0f3";
@@ -17,17 +12,9 @@ export default {
     xas:api.baseUrl+ "/api/accounts/getBalance/",
     baseUrl:api.baseUrl,
     dappid:api.dappId,
-    Upload:dappBaseUrl+"/UpLoader",
     loginApi:api.baseUrl+"/api/accounts/open2",
     registerApi:api.baseUrl+"/api/accounts/new",
     tixian:dappBaseUrl+"/transactions/signed",
-    areas:dappBaseUrl+"/areas",
-    citys:dappBaseUrl+"/citys",
-    bind:dappBaseUrl+"/bind",
-    logs:dappBaseUrl+"/logs",
-    myareas:dappBaseUrl+"/myarea",
-    isBind:dappBaseUrl+"/isBind",
-    lands:dappBaseUrl+"/lands",
     UiaTrans:api.baseUrl+"/peer/transactions",
     assetTransfers:api.baseUrl+"/api/uia/transactions/my",
     UiaMessage:api.baseUrl+"/api/uia/balances/",
@@ -35,12 +22,6 @@ export default {
     balances:dappBaseUrl+"/balances/",
     getTransation:dappBaseUrl+"/transactions",
     transfers:dappBaseUrl+"/transfers",
-    contracts:dappBaseUrl+"/contracts ",
-    getFiles:dappBaseUrl+"/transbyfile",
-    getFiles2:dappBaseUrl+"/transbyfile2",
-    getFiles3:dappBaseUrl+"/transbyfile3",
-    CheckDistinct:dappBaseUrl+"/CheckDistinct",
-    cy:"dreamworld.DDC",
-    //cy:"Dream.DDC",
+    cy: "ubiquity.USO",
     magic:magic_mainnet,
 }

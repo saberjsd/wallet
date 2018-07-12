bbb62e7d5e2fa72b7d8be1296044eb562a1bffb1
DjgMyGit <643716244@qq.com>
2018-07-12 15:47:29 +0800

    change the uso name

--- src/views/home/person/transcation_uia.vue
+++ src/views/home/person/transcation_uia.vue
@@ -79,7 +79,7 @@ export default {
         currency: "",
         message:""
       },
-      uia: [{currency:"XAS"},{currency:Config.cy}],
+      uia: [{currency:"XAS"},{currency:"USO"}],
       rules2: {
         address: [{ validator: checkAddress, trigger: "blur" }],
         amount: [{ validator: cheackAmount, trigger: "blur" }]
@@ -96,7 +96,7 @@ export default {
     if(cuy=="XAS"){
       this.formInline.currency="XAS";
     }else{
-      this.formInline.currency=Config.cy;
+      this.formInline.currency="USO";
     }
     // this.getUia();
   },
